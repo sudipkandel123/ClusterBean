6ab0607ff9affdf72b73405a5340b896a507a085
Sudip <sudip@sudips-macbook-pro.local>
2018-12-14 21:58:59 +0530

    Initial Commit

---
+++ ClusterBean MapViewUITests/ClusterBean_MapViewUITests.swift
@@ -0,0 +1,34 @@
+//
+//  ClusterBean_MapViewUITests.swift
+//  ClusterBean MapViewUITests
+//
+//  Created by Sudip on 12/14/18.
+//  Copyright © 2018 Sudeepasa. All rights reserved.
+//
+
+import XCTest
+
+class ClusterBean_MapViewUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
